d6c2f2d3a9852957c33f31f4531eca6e3fbebd96
Shashank15DV <sashank@data-vector.com>
2024-09-29 20:53:48 +0530

    --

--- src/start-server.ts
+++ src/start-server.ts
@@ -1,9 +1,10 @@
 import app from "./server";
 
 const StartServer = () => {
-    app.listen(process.env.PORT || 3001, () => {
-        console.log("Server Started");
-    });
-}
+  console.log(process.env.PORT);
+  app.listen(process.env.PORT || 3001, () => {
+    console.log("Server Started");
+  });
+};
 
-export default StartServer
+export default StartServer;
